ebc90d9ac0280225c96aecef959ff48199249db9
dev <166125538+Purohitdev@users.noreply.github.com>
2025-02-02 14:11:29 +0530

    footer-responsive  texture-add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="max-h-[70vh] bg-[#161818]  fixed bottom-0 left-0 w-full text-white  py-16 px-16  ">
-      <h1 className='font-black text-[12rem] leading-none '>JU VERSE.</h1>
+<footer className="max-h-[70vh] bg-[#161818] fixed bottom-0 left-0 w-full text-white py-16 px-4 md:px-16">
+      <h1 className='font-black text-[4rem] sm:text-[4rem] md:text-[6rem] lg:text-[12rem] leading-none'>JU VERSE.</h1>
       <div className="min-h-[20vh] flex items-center gap-5 mt-16 ">
 
         <div className=" w-full min-h-[20vh] ">
@@ -26,14 +26,14 @@ function Footer() {
           <ol>telegram</ol>
 
         </div>
-        <div className=" w-full min-h-[20vh] flex justify-end">
+        <div className="w-0 hidden sm:flex sm:w-full min-h-[20vh] justify-end">
 
           <div className="border border-[#ffffff57] w-[80%] rounded-2xl overflow-hidden ">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.097329012138!2d75.87261508346361!3d26.775533916727163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396dc892b8f05399%3A0x43fbca7105eca800!2sJECRC%20University!5e1!3m2!1sen!2sin!4v1738243665873!5m2!1sen!2sin"
               width="100%"
               height="100%"
-              style={{ border: 0 }} 
+              style={{ border: 0 }}
               allowFullScreen
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade">
